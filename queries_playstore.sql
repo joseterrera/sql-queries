bece44edbd31148da948d4058911288fd64a0836
Jose Terrera <joseterrera@yahoo.com>
2020-07-19 22:06:58 -0700

    finish exercises

--- queries_playstore.sql
+++ queries_playstore.sql
@@ -11,3 +11,95 @@ SELECT category, COUNT(*) FROM analytics GROUP BY category;
 
 -- Find the top 5 most-reviewed apps and the number of reviews for each.
 SELECT * FROM analytics ORDER BY reviews DESC LIMIT 5;
+
+-- Find the app that has the most reviews with a rating greater than equal to 4.8.
+SELECT * FROM analytics 
+WHERE reviews >= 4.8
+ORDER BY reviews DESC
+LIMIT 1;
+
+-- Find the average rating for each category ordered by the highest rated to lowest rated.
+SELECT category, AVG(rating) FROM analytics
+GROUP BY category
+ORDER BY AVG DESC;
+
+-- Find the name, price, and rating of the most expensive app with a rating that’s less than 3.
+SELECT app_name, price, rating FROM analytics
+WHERE rating < 3
+ORDER BY price DESC
+LIMIT 1;
+
+-- Find all apps with a min install not exceeding 50, that have a rating. Order your results by highest rated first.
+SELECT * FROM analytics
+  WHERE min_installs <= 50 
+    AND rating IS NOT NULL
+  ORDER BY rating DESC;
+
+
+-- Find the names of all apps that are rated less than 3 with at least 10000 reviews.
+SELECT * FROM analytics
+  WHERE rating < 3 AND reviews >= 1000;
+
+-- Find the top 10 most-reviewed apps that cost between 10 cents and a dollar.
+SELECT * FROM analytics
+  WHERE price BETWEEN 0.1 AND 1
+  ORDER BY reviews DESC
+  LIMIT 10;
+
+-- Find the most out of date app. Hint: You don’t need to do it this way, but it’s possible to do with a subquery: http://www.postgresqltutorial.com/postgresql-max-function/
+SELECT * FROM analytics
+  WHERE last_updated = (SELECT MIN(last_updated) FROM analytics);
+
+-- or:
+
+SELECT * FROM analytics
+  ORDER BY last_updated
+  LIMIT 1;
+
+-- Find the most expensive app (the query is very similar to #11).
+SELECT * FROM analytics
+  WHERE price = (SELECT MAX(price) FROM analytics);
+
+-- or
+SELECT * FROM analytics
+  ORDER BY price DESC
+  LIMIT 1;
+
+-- Count all the reviews in the Google Play Store.
+SELECT SUM(reviews) AS "All Reviews" FROM analytics;
+
+-- Find all the categories that have more than 300 apps in them.
+SELECT category FROM analytics
+  GROUP BY category
+  HAVING COUNT(*) > 300;
+
+  -- Find the app that has the highest proportion of min_installs to reviews, among apps that have been installed at least 100,000 times. Display the name of the app along with the number of reviews, the min_installs, and the proportion.
+
+
+  SELECT app_name, reviews min_installs, min_installs/reviews AS proportion FROM analytics
+  WHERE min_installs >= 100000
+  ORDER BY proportion DESC
+  LIMIT 1;
+
+  -- Find the name and rating of the top rated apps in each category, among apps that have been installed at least 50,000 times.
+SELECT app_name, rating, category FROM analytics
+  WHERE (rating, category) in (
+    SELECT MAX(rating), category FROM analytics
+      WHERE min_installs >= 50000
+      GROUP BY category
+    )
+  ORDER BY category;
+
+--Find all the apps that have a name similar to "facebook".
+
+SELECT * FROM analytics 
+  WHERE app_name ILIKE '%facebook%';
+
+
+--Find all the apps that have more than 1 genre.
+SELECT * FROM analytics 
+  WHERE  array_length(genres, 1) = 2;
+
+-- Find all the apps that have education as one of their genres.
+SELECT * FROM analytics 
+  WHERE genres @> '{"Education"}';
